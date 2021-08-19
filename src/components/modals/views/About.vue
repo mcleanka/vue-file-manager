1769620e3ae036375cb21595de9b3a7e65a8a634
mcleanka <mcleankasambala@gmail.com>
2021-08-19 22:09:12 +0200

    Add:: added about section info

--- src/components/modals/views/About.vue
+++ src/components/modals/views/About.vue
@@ -1,42 +1,53 @@
 <template>
-    <div class="modal-content fm-modal-about">
-        <div class="modal-header">
-            <h5 class="modal-title">{{ lang.modal.about.title }}</h5>
-            <button type="button" class="close" aria-label="Close" v-on:click="hideModal">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-            <strong>{{ lang.modal.about.name }}</strong>
-            <hr>
-            <dl class="row">
-                <dt class="col-3">{{ lang.modal.about.version }}:</dt>
-                <dd class="col-9">{{ version }}</dd>
-
-                <dt class="col-3">{{ lang.modal.about.developer }}:</dt>
-                <dd class="col-9">Aleksandr Manekin (alexusmai@gmail.com)</dd>
-
-                <dt class="col-3">GitHub:</dt>
-                <dd class="col-9">
-                    <a href="https://github.com/alexusmai/laravel-file-manager" target="_blank">
-                        alexusmai/laravel-file-manager
-                    </a>
-                    <br>
-                    <a href="https://github.com/alexusmai/vue-laravel-file-manager" target="_blank">
-                        alexusmai/vue-laravel-file-manager
-                    </a>
-                </dd>
-            </dl>
-        </div>
+  <div class="modal-content fm-modal-about">
+    <div class="modal-header">
+      <h5 class="modal-title">{{ lang.modal.about.title }}</h5>
+      <button
+        type="button"
+        class="close"
+        aria-label="Close"
+        v-on:click="hideModal"
+      >
+        <span aria-hidden="true">&times;</span>
+      </button>
     </div>
+    <div class="modal-body">
+      <strong>{{ lang.modal.about.name }}</strong>
+      <hr />
+      <dl class="row">
+        <dt class="col-3">{{ lang.modal.about.version }}:</dt>
+        <dd class="col-9">{{ version }}</dd>
+
+        <dt class="col-3">{{ lang.modal.about.developer }}:</dt>
+        <dd class="col-9">Mclean Kasambala (mcleankasambala@gmail.com)</dd>
+
+        <dt class="col-3">GitHub:</dt>
+        <dd class="col-9">
+          <a
+            href="https://github.com/mcleanka/laravel-file-manager"
+            target="_blank"
+          >
+            mcleanka/laravel-file-manager
+          </a>
+          <br />
+          <a
+            href="https://github.com/mcleanka/vue-file-manager"
+            target="_blank"
+          >
+            mcleanka/vue-file-manager
+          </a>
+        </dd>
+      </dl>
+    </div>
+  </div>
 </template>
 
 <script>
-import modal from '../mixins/modal';
-import translate from '../../../mixins/translate';
+import modal from "../mixins/modal";
+import translate from "../../../mixins/translate";
 
 export default {
-  name: 'About',
+  name: "About",
   mixins: [modal, translate],
   computed: {
     /**
